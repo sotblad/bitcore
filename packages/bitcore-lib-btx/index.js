b339473081b6e9e841812b063a616c5055adc2aa
dalijolijo <david.bergen@gmx.net>
2019-05-04 17:19:58 +0000

    Skip check if lib repo exists in subdirs

--- packages/bitcore-lib-btx/index.js
+++ packages/bitcore-lib-btx/index.js
@@ -4,7 +4,7 @@ var bitcore = module.exports;
 
 // module information
 bitcore.version = 'v' + require('./package.json').version;
-bitcore.versionGuard = function(version) {
+bitcore.versionGuard = function(version) { return;
   if (version !== undefined) {
     var message = 'More than one instance of bitcore-lib-btx found. ' +
       'Please make sure to require bitcore-lib-btx and check that submodules do' +
